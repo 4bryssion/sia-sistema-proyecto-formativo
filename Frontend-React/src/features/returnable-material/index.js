b49ac7840dc81fb8682db936b8025ac2ba58be16
4bryssion <acevedomedinasantiago1234@gmail.com>
2026-05-25 14:38:49 -0500

    feat(structure): The structure of the returnable materials module was created

--- Frontend-React/src/features/returnable-material/index.js
+++ Frontend-React/src/features/returnable-material/index.js
@@ -1,1 +1,3 @@
 export { default as CreateReturnableMaterialPage } from "./pages/CreateReturnableMaterialPage";
+
+export { default as ListReturnableMaterialPage } from "./pages/ListReturnableMaterialPage";
