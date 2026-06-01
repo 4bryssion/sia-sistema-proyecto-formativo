be13825e098ef44b5b31eb93b68fd37dd382be30
Sofia Cardona <sofiacardonar16@gmail.com>
2026-06-01 08:34:35 -0500

    feat(users): initialize report structure for users module

--- Frontend-React/src/features/users/pages/ListUserPage.jsx
+++ Frontend-React/src/features/users/pages/ListUserPage.jsx
@@ -3,14 +3,14 @@ import { UserColumns } from "../table/UserColumns"
 import { users } from "../data/users"
 import { Link, useNavigate } from "react-router-dom";
 import { Undo2 } from "lucide-react";
-// import { useState } from "react";
+import { useState } from "react";
 
-// import ReportConfigModal from "../reports/components/ReportConfigModal.jsx"
+import ReportConfigModal from "../report/components/ReportConfigModal.jsx"
 
 export default function ListUserPage() {
 
     const navigate = useNavigate();
-    // const [isReportModalOpen, setIsReportModalOpen] = useState(false);
+    const [isReportModalOpen, setIsReportModalOpen] = useState(false);
 
   return (
     <div className="p-6">
@@ -40,12 +40,12 @@ export default function ListUserPage() {
 
             <div className="grid sm:flex gap-12 items-center">
                 
-                {/* <Button
+                <Button
                     variant="secondary" 
                     onClick={() => setIsReportModalOpen(true)}
                 >
                     Generar Reporte
-                </Button> */}
+                </Button>
 
 
                 <Link to="/users/create">
@@ -66,10 +66,10 @@ export default function ListUserPage() {
         columns={UserColumns}
       />
 
-      {/* <ReportConfigModal
+      <ReportConfigModal
         isOpen={isReportModalOpen}
         onClose={() => setIsReportModalOpen(false)}
-      /> */}
+      />
 
 
     </div>
---
+++ Frontend-React/src/features/users/report/config/userReportFields.js
@@ -0,0 +1,70 @@
+export const userReportFields = [
+{
+    key: "nombre",
+    label: "Nombre",
+    default: true,
+},
+//Esto luego se soluciona por que en el crear 
+// solo recibe nombre pero en el back estan separados
+// {
+//     key: "apellido",
+//     label: "Apellido",
+//     default: true,
+// },
+{
+    key: "rol",
+    label: "Rol",
+    default: true,
+},
+{
+    key: "tipo_documento",
+    label: "Tipo de documento",
+    default: true,
+},
+{
+    key: "numero_documento",
+    label: "Documento",
+    default: true,
+},
+{
+    key: "fecha_finalizacion",
+    label: "Fecha de finalización",
+    default: true,
+},
+{
+    key: "email",
+    label: "Email",
+    default: true,
+},
+{
+    key: "telefono",
+    label: "Teléfono",
+    default: false,
+},
+{
+    key: "segundo_telefono",
+    label: "segundo teléfono",
+    default: false,
+},
+{
+    key: "direccion",
+    label: "Dirección",
+    default: false,
+},
+{
+    key: "estado",
+    label: "Estado",
+    default: true,
+},
+{
+    key: "foto",
+    label: "Foto",
+    default: false,
+},
+{
+    key: "cuentadante",
+    label: "Cuentadante",
+    default: true,
+},
+
+];
